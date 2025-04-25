f5d32caba76a5bba7d1a736cb3cde80efc05cbe6
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2025-04-25 14:56:41 +0800

    Change to support .env in config and changed .gitignore

--- backend/src/mikro-orm.config.ts
+++ backend/src/mikro-orm.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from '@mikro-orm/postgresql';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: '123',
-  dbName: 'chumcheck',
+  host: process.env.DB_HOST,
+  port: +(process.env.DB_PORT || 5432),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
   entities: ['./dist/**/*.entity.js'],
   entitiesTs: ['./src/**/*.entity.ts'],
   debug: true,
