70ad02800d2c754aa3d712ae1fea96d4c951f7ff
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2025-05-02 22:03:27 +0800

    temp stop kay ga valo

--- backend/src/rna/rna.controller.ts
+++ backend/src/rna/rna.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Get,
+    Param,
+    ParseIntPipe,
+    Post,
+    Query,
+    UseGuards,
+  } from '@nestjs/common';
+  import { JwtGuard } from 'src/auth/guard';
+  import { UratQuestionAnswer } from 'src/entities/urat-question-answer.entity';
+import { RnaService } from './rna.service';
 
 @Controller('rna')
-export class RnaController {}
+export class RnaController {
+    constructor(private readonly rnaService: RnaService) {}
+
+    @Get()
+    async getRNAbyId(
+        @Query('startupId', ParseIntPipe) startupId: number,
+      ) {
+        return this.rnaService.getRNAbyId(startupId);
+      }
+    
+}
--- backend/src/rna/rna.service.ts
+++ backend/src/rna/rna.service.ts
@@ -1,4 +1,11 @@
+import { EntityManager } from '@mikro-orm/core';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class RnaService {}
+export class RnaService {
+    constructor(private em: EntityManager) {}
+    
+  async getRNAbyId(startupId: number) {
+    return await this.em.findOne(Startup, { id: startupId });
+  }
+}
