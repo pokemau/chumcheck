218055206596e5639fda5a9e447fc3c53ab71671
pokemau <rentillosa90@gmail.com>
2025-06-16 21:26:48 +0800

    cors fix 2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -66,6 +66,7 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:5173', 'https://chumcheck.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
