a72078ee58c7b4839d3c6fd2ff5bff5f2997d39b
pokemau <rentillosa90@gmail.com>
2025-09-18 21:45:41 +0800

    remove unused, and move scripts

--- client/.next/static/chunks/src_app_page_tsx_186a79fe._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/9bf22_next_dist_70c66a70._.js"
-],
-    source: "dynamic"
-});
