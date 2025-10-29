ab38e36a8e5a1b6529bb4d0577443ac277f71c85
minagawaYuki <markadrian.baring@cit.edu>
2025-10-29 16:37:01 +0800

    update entity fields

--- backend/src/entities/rna.entity.ts
+++ backend/src/entities/rna.entity.ts
@@ -6,32 +6,32 @@ import {
   } from '@mikro-orm/core';
 import { ReadinessLevel } from './readiness-level.entity';
 import { Startup } from './startup.entity';
-  
+
   @Entity({ tableName: 'rna' })
   export class StartupRNA {
     @PrimaryKey()
     id!: number;
-  
+
     @ManyToOne(() => ReadinessLevel, {
       fieldName: 'readiness_level_id',
     })
     readinessLevel!: ReadinessLevel;
-  
+
     @ManyToOne(() => Startup, {
       fieldName: 'startup_id',
       deleteRule: 'cascade',
     })
     startup!: Startup;
-  
+
     @Property({ fieldName: 'is_ai_generated', type: 'boolean' })
     isAiGenerated!: boolean;
-  
+
     @Property({ type: 'text' })
     rna!: string;
-  
+
     @Property({ fieldName: 'created_at' })
     createdAt?: Date = new Date();
-  
+
     @Property({ fieldName: 'updated_at', onUpdate: () => new Date() })
     updatedAt?: Date = new Date();
   }
