eb657036432dcd76ee13f6a3a670b3240041d607
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2025-12-01 17:50:22 +0800

    fix: commented options for dev

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,8 +15,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'https://launchup.vercel.app',
-      // 'http://localhost:5173',
-      // 'http://127.0.0.1:5173',
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
