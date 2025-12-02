8709b3632480c22eee6f927076c6a51606d4fa11
Joemarie Amparo <144643361+jomeamparo@users.noreply.github.com>
2025-12-02 21:24:50 +0800

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'https://launchup.vercel.app',
+      'https://launchup.onrender.com',
       // 'http://localhost:5173',
       // 'http://127.0.0.1:5173',
     ],
